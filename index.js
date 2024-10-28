a8da11738510e7a7883e0c50be91c229fb6427ef
Rajeev Telagasetti <telagasettirajeev999@gmail.com>
2024-10-28 11:11:31 +0000

    final commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+let multer = require("multer");
 
 var app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use( express.urlencoded({extended: true}) );
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
@@ -12,6 +15,20 @@ app.get('/', function (req, res) {
 });
 
 
+const upload = multer({dest: "uploads/"});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res)=>{
+  let file = req.file ;
+
+  if (!file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+console.log( file );
+
+  res.json( {name: file.originalname, type: file.mimetype, size: parseInt(file.size) } );
+
+})
 
 
 const port = process.env.PORT || 3000;
